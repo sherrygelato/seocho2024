e9f423761c0fe1e74eaef1c1a5ca0a5cade88eef
sherrygelato <sherrygelato@kakao.com>
2024-05-21 10:49:32 +0900

    [+] 연산 ++, --

--- js/operator.js
+++ js/operator.js
@@ -2,5 +2,12 @@
 let a = 1;
 let b = 2;
 
-const c = (a++, b++);
-console.log("그렇다면 c =", c);
+const c = (a++, b++); // ++은 return보다 늦다
+console.log("1) c =", c);
+console.log("2) a =", a);
+console.log("3) b =", b);
+
+// console.log("==========");
+
+const d = (a--, b + a);
+console.log("4) d =", d);
