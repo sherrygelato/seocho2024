73d1acace09e7433690f7d2680e80d704b3c92ee
sherrygelato <sherrygelato@kakao.com>
2024-05-21 11:49:13 +0900

    [+] add boolean.js

---
+++ js/boolean.js
@@ -0,0 +1,21 @@
+// const first = '';
+const first = 'Uncle';
+const last = 'Bob';
+
+console.log(`${first} ${last}`);
+
+console.log('-------- if문');
+
+if (first) {
+    console.log(first);
+} 
+console.log(last);
+
+console.log('-------- 3항 연산자');
+
+let fullName = first ? first + ' ' : ''
+fullName = fullName + last;
+console.log('fullName = ', fullName);
+
+console.log(`Good case : ${first ? first + ' ' : ''}${last}`); // Good!
+console.log(`Great case : ${first}${first && ' '}${last}`); // Great!
--- js/operator.js
+++ js/operator.js
@@ -6,28 +6,28 @@ for (let i = 0; i < 10; i++) { // 예전엔 이렇게 썼지만
     console.log(i);
 }
 
-console.log("==========");
+console.log('==========');
 
 for (let i = 0; i < 10; i = i + 1) { // 요즘은 이렇게 씀
     console.log(i);
 }
 
-console.log("==========");
+console.log('==========');
 
 let c = (a++, b++); // ++은 return보다 늦고, 최근들어 잘 쓰지 않음
 // let c = ((a = a + 1), (b = b + 1));
 // 오해를 불러일으키는 코드는 좋지 않은 코드다.
 
-console.log("1) c =", c);
-console.log("2) a =", a);
-console.log("3) b =", b);
+console.log('1) c =', c);
+console.log('2) a =', a);
+console.log('3) b =', b);
 
 let d = (a--, b + a);
 
-console.log("4) d =", d);
+console.log('4) d =', d);
 
 d = void (c = a + b); 
 // void 연산자 (평가/실행 후 undefined 반환)
-console.log("5) a =", a, ", b =", b, ", c =", c, ", d =", d); 
+console.log('5) a =', a, ', b =', b, ', c =', c, ', d =', d); 
 // Assignment to constant variable.
 // const => let으로 바꾸기(c, d)
