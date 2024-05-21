672ca027dcb30d49395cf62cf13b7f60b68467df
sherrygelato <sherrygelato@kakao.com>
2024-05-21 11:27:55 +0900

    [+] void 추가

--- js/operator.js
+++ js/operator.js
@@ -14,15 +14,19 @@ for (let i = 0; i < 10; i = i + 1) { // 요즘은 이렇게 씀
 
 console.log("==========");
 
-const c = (a++, b++); // ++은 return보다 늦고, 최근들어 잘 쓰지 않음
-// const c = ((a = a + 1), (b = b + 1));
+let c = (a++, b++); // ++은 return보다 늦고, 최근들어 잘 쓰지 않음
+// let c = ((a = a + 1), (b = b + 1));
 // 오해를 불러일으키는 코드는 좋지 않은 코드다.
 
 console.log("1) c =", c);
 console.log("2) a =", a);
 console.log("3) b =", b);
 
-const d = (a--, b + a);
+let d = (a--, b + a);
 
 console.log("4) d =", d);
 
+d = void (c = a + b); 
+console.log("5) a =", a, ", b =", b, ", c =", c, ", d =", d); 
+// Assignment to constant variable.
+// const => let으로 바꾸기(c, d)
