537d815ca6db626cbc4e22c48c2ce1a877bbf688
sherrygelato <sherrygelato@kakao.com>
2024-05-23 10:13:07 +0900

    숙제 커밋:

---
+++ hw/20240523.js
@@ -0,0 +1,36 @@
+// 슬라이드 65~67
+
+// // 연습문제 1
+// user 객체를 받아서 id와 name을 출력하는 함수를 3개의 함수로 작성하시오.
+// (Function signature를 3개 이상으로 표현하기)
+
+
+// const hong = {id: 1, name: 'Hong'};
+// const lee = {id: 2, name: 'Lee'};
+// f1(hong);  f2
+// ⇒ 1, 'Hong'        console.log(id, name)
+
+
+
+
+
+// // 연습문제 2
+// 다음 user 객체에서 passwd 프로퍼티를 제외한 데이터를 userInfo 라는 변수에 할당하시오.
+
+// const user = {id: 1, name: 'Hong', passwd: 'xxx', addr: 'Seoul'}
+// const {passwd, ...userInfo} = user;
+// console.log(userInfo); 
+// // 출력결과: {id: 1, name: 'Hong', addr: 'Seoul'}
+
+
+
+
+
+// // 연습문제 3
+// 다음 arr에서 3개의 id를 id1, id2, id3로 할당하시오. 
+
+// const arr = [[{id: 1}], [{id:2}, {id: 3}]]
+// cf. const id1 = arr[0][0].id;
+// const [ [{id:id1}], [{id:id2}, {id:id3} ] ] = arr;
+// console.log(id1, id2, id3); 
+// // 출력결과: 1 2 3
