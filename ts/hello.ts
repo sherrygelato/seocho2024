0331d390e19fb82ad9f3b7ab51cec2e9af2ec5af
sherrygelato <sherrygelato@kakao.com>
2024-06-13 09:06:52 +0900

    ts 타입 별칭,  type & interface

--- ts/hello.ts
+++ ts/hello.ts
@@ -1,3 +1,5 @@
+import {v4 as uuidv4} from 'uuid'; 
+
 const myName = 'Sico';
 // let s: string | number = 'abc';
 let s = 'abc'; // type check system이 타입 정의, 안적을 수록 추론이 확실히 되는 건 ㄴㄴ
@@ -145,3 +147,114 @@ f(f3); // OK
 console.log('f(f3)', f(f3));
 // f(f4); // strictFunctionTypes가 false라면 OK, 아니면 Fail!
 // console.log('f(f4)', f(f4));
+
+const introduce = (name: string, height?: number) => {
+    console.log(`이름 : ${name}`);
+    if (height) console.log(`키 : ${height + 10}`); // not bad
+    if (typeof height === 'number') console.log(`키 : ${height + 10}`); // great
+    // error : 'height' is possibly 'undifined'
+}
+
+introduce('홍길동');
+introduce('홍길동', undefined);
+introduce('홍길동', 170);
+
+const introduce2 = (name:string, 
+    height : number|undefined ) => { // 반드시 height를 넣어야 함
+    console.log(`이름 : ${name}`);
+    if(typeof height === 'number'){
+        console.log(`키 : ${height + 10}`)   
+    }
+}
+    
+const introduce3  = (name : string, age : number, height?:number, ) => { // optional parameter는 항상 뒤에!
+    console.log(`이름 :  ${name}`);
+    console.log(`나이 : ${age} 살`);
+    if(height){
+        return console.log(`키 ${height+10}cm`);   
+    }
+}
+
+// return type은 parameter 뒤에 넣어야 한다.
+var xfn = (x: number): number => x ** 2;
+function xfn1(x: number) : number {
+    return x ** 2;
+}
+
+
+
+let songLogger : (song:string) => void;
+
+songLogger = (song) => {
+    console.log(`${song}`);
+    return true;  // OK
+}
+songLogger("HeartBeat"); // OK
+
+const b = songLogger("HeartBeat");
+console.log(`void 인데 b값 나오나:`, b);
+
+// if (songLogger("HeartBeat")) // Error!
+//   console.log('*******');
+
+// 선언부
+function addFn(a : number, b: number) : number;
+function addFn(a : number, b: number, c: number) : number;
+
+// 구현부
+function addFn(a : number, b: number, c?: number) {
+    if (typeof c === 'number') return a * b * c;
+    return a + b;
+}
+
+// 마치 다른 함수인 것처럼
+console.log(`addFn(1, 2):`, addFn(1, 2));
+console.log(`addFn(1, 2, 3):`, addFn(1, 2, 3));
+
+
+
+// 연습문제
+const SIZE = [
+    {id: 'XS', price: 8000},
+    {id: 'S', price: 10000},
+    {id: 'M', price: 12000},
+    {id: 'L', price: 14000},
+    {id: 'XL', price: 15000},
+] as const;
+
+const sizeOption = {XS: 1, S: 5, M: 2, L: 2, XL: 4};
+
+// const totalPrice = SIZE.reduce((currPrice, size) =>
+//     currPrice + (sizeOption[size.id] * size.price), 0
+// ); // 에러 해결하는 게 문제
+  
+const totalPrice = SIZE.reduce((currPrice, size) =>
+    currPrice + (sizeOption[size.id] * size.price), 0
+);
+
+console.log(uuidv4());
+
+// 연습문제 type Ud2 = (TUser | TDept) & {addr: string};
+
+interface TUser {
+    id: number;
+    name: string;
+  }
+  
+  interface Dept {
+    id: number;
+    dname: string;
+    captain: string;
+  }
+  interface Ud2 {
+    id: number;
+    name?: string;
+    dname?: string;
+    captain?: string;
+    // <이 부분을 작성하시오>
+    addr: string;
+  }
+  
+  // 다음 코드가 오류가 없으면 통과!
+  const ud2: Ud2 = {id: 1, name: 'HH', addr: 'Seoul'};
+  const ud3: Ud2 = {id: 1, dname: 'HH', captain: 'HH', addr: 'Seoul'};
