99221619549125da5b0c1c89d95705b781789d56
sherrygelato <sherrygelato@kakao.com>
2024-07-08 09:34:46 +0900

    next-auth 시작

--- next/my-app/app/layout.tsx
+++ next/my-app/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           </Button>
         </div>
         <div className='container flex min-h-screen flex-col items-center justify-between p-24'>
-          {children}
+          <div className='mt-7'>{children}</div>
         </div>
       </body>
     </html>
