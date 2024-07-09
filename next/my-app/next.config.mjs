a357f49650f244d79c4cf257f26400a338712d93
sherrygelato <sherrygelato@kakao.com>
2024-07-09 09:54:52 +0900

    todo page 추가

--- next/my-app/next.config.mjs
+++ next/my-app/next.config.mjs
@@ -1,15 +1,45 @@
 /** @type {import('next').NextConfig} */
+const isReal = process.env.NODE_ENV === 'production';
+
+const images = {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'via.placeholder.com',
+      port: '',
+      pathname: '/**',
+    },
+  ],
+};
+
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+  images,
+  compiler: {
+    removeConsole: {
+      exclude: ['error', 'debug'],
+    },
   },
 };
 
-export default nextConfig;
+const nextConfigDev = {
+  images,
+  compiler: {},
+};
+
+export default isReal ? nextConfig : nextConfigDev;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'via.placeholder.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
