c1fa3261ac85f3c9b2391c7dd5fe1033d5c4da16
sherrygelato <sherrygelato@kakao.com>
2024-07-16 09:59:41 +0900

    update api

--- next/bookmark/app/api/books/route.ts
+++ next/bookmark/app/api/books/route.ts
@@ -1,49 +1,38 @@
+import { execute, query } from '@/lib/db';
+import { Book } from '@/lib/types';
+import { RowDataPacket } from 'mysql2';
 import { NextRequest, NextResponse } from 'next/server';
-import { query } from '@/lib/db';
 
+export async function POST(req: NextRequest) {
+  const { title, owner, clickdel } = await req.json();
+  const rows = await execute(
+    'insert into Book(title, owner, clickdel) values(?,?,?)',
+    [title, owner, clickdel]
+  );
+
+  const [book] = await query<Book & RowDataPacket>(
+    'select * from Book where id = ?',
+    [rows.insertId]
+  );
+
+  return NextResponse.json({ book });
+}
+
+// book list for login user
 export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const userId = searchParams.get('userId');
-
+  // console.log('🚀  userId:', userId);
+  // const conn = await mysql.createConnection(config);
+  // const conn = await pool.getConnection();
   try {
-    const [books] = await query(`select * from Book where owner=?`, [userId]);
-    return NextResponse.json({
-      userId,
-      message: 'OK',
-      books,
-    });
+    const books = await query('select * from Book where owner = ?', [userId]);
+
+    return NextResponse.json({ books });
   } catch (error) {
     const message = error instanceof Error ? error.message : 'error!';
-    return NextResponse.json({ userId, message });
+    return NextResponse.json({ userId, message }, { status: 500 });
   }
-}
-
-export async function POST(req: NextRequest) {
-  const { searchParams } = req.nextUrl;
-  const userId = searchParams.get('userId');
-
-  const { title, owner } = await req.json();
-
-  const rows = await query(`insert into Book(title, owner) value(?, ?)`, [
-    title,
-    owner,
-  ]);
-
-  return NextResponse.json({
-    userId,
-    message: 'OK',
-    rows,
-  });
-}
-
-export async function DELETE(req: NextRequest) {
-  const { searchParams } = req.nextUrl;
-  const userId = searchParams.get('userId');
-
-  const [books] = await query(`delete from Book where owner=?`, [userId]);
-  return NextResponse.json({
-    userId,
-    message: 'OK',
-    books,
-  });
+  // conn.end(); // close
+  // conn.release(); // release to pool
 }
--- next/bookmark/lib/db.ts
+++ next/bookmark/lib/db.ts
@@ -1,5 +1,4 @@
-import mysql from 'mysql2/promise';
-import { parseArgs } from 'util';
+import mysql, { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 
 const {
   DB_HOST: host,
@@ -17,25 +16,38 @@ export const config = {
   connectionLimit: 3,
   maxIdle: 3,
   idleTimeout: 60000,
-
-  //   connectionLimit: 10,
-  //   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  //   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  //   queueLimit: 0, // 무한대
-  //   enableKeepAlive: true, // 연결을 close할 때까지
-  //   keepAliveInitialDelay: 0,
 };
 
 const pool = mysql.createPool(config);
 
 // key point: release to pool
-export const query = async (sql: string, params: unknown[]) => {
+export const query = async <T extends RowDataPacket>(
+  sql: string,
+  params: unknown[]
+) => {
   const conn = await pool.getConnection();
 
   try {
-    const [rows, fields] = await conn.query(sql, params);
+    const [rows] = await conn.query<T[]>(sql, params);
     console.table({ rows });
-    return [rows, fields];
+    return [rows];
+  } catch (error) {
+    console.table({ error });
+    throw error;
+  } finally {
+    conn.release();
+  }
+};
+
+// to write
+export const execute = async (sql: string, params: unknown[]) => {
+  const conn = await pool.getConnection();
+
+  try {
+    const [rows] = await conn.query<ResultSetHeader>(sql, params);
+    console.log(rows.insertId);
+    console.table({ rows });
+    return rows;
   } catch (error) {
     console.table({ error });
     throw error;
